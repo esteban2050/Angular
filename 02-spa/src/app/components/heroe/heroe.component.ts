455a9f8f6bb6d4e970c7e472710ce6e9dd94597d
Juan Esteban <ju.eslogi@hotmail.com>
2019-06-12 11:20:15 -0500

    Ventana detallada del heroe
    Se finaliza la ventana donde aparece toda la informacion del heroe

--- 02-spa/src/app/components/heroe/heroe.component.ts
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from "../../Services/heroes.service";
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
+  heroe: any= {};
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,
+               private heroesService: HeroesService ) {
 
-  ngOnInit() {
-  }
+    this.activatedRoute.params.subscribe( params=>{
+      this.heroe = this.heroesService.getHeroe( params['id'] );
+      console.log(this.heroe);
+    } )
+   }
+
 
 }
--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -10,9 +10,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
   constructor(private _heroesService: HeroesService,
-              private router: Router
-    ) {
-    console.log('constructor');
+              private router: Router) {
    }
 
   ngOnInit() {
